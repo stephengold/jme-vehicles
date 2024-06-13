3d7761d72b699ede2370882a80c00e64c6272a15
stephengold <sgold@sonic.net>
2024-06-12 17:57:44 -0700

    PropType:  entities in the javadoc

--- MavDemo2/src/main/java/com/jayfella/jme/vehicle/niftydemo/state/PropType.java
+++ MavDemo2/src/main/java/com/jayfella/jme/vehicle/niftydemo/state/PropType.java
@@ -10,67 +10,70 @@ public enum PropType {
     // values
 
     /**
-     * construction barrel with an orange base, from "Barrier & Traffic Cone
+     * construction barrel with an orange base, from "Barrier &amp; Traffic Cone
      * Pack"
      */
     Barrel1,
     /**
-     * construction barrel with a black base, from "Barrier & Traffic Cone Pack"
+     * construction barrel with a black base, from "Barrier &amp; Traffic Cone
+     * Pack"
      */
     Barrel2,
     /**
-     * Jersey barrier painted black, from "Barrier & Traffic Cone Pack"
+     * Jersey barrier painted black, from "Barrier &amp; Traffic Cone Pack"
      */
     Barrier,
     /**
-     * Jersey barrier painted black and orange, from "Barrier & Traffic Cone
+     * Jersey barrier painted black and orange, from "Barrier &amp;Traffic Cone
      * Pack"
      */
     BarrierPainted,
     /**
-     * traffic cone with a black base, from "Barrier & Traffic Cone Pack"
+     * traffic cone with a black base, from "Barrier &amp; Traffic Cone Pack"
      */
     Cone1,
     /**
-     * traffic cone with an orange base, from "Barrier & Traffic Cone Pack"
+     * traffic cone with an orange base, from "Barrier &amp; Traffic Cone Pack"
      */
     Cone2,
     /**
-     * Jersey barrier topped with chain-link fence, from "Barrier & Traffic Cone
-     * Pack"
+     * Jersey barrier topped with chain-link fence, from "Barrier &amp; Traffic
+     * Cone Pack"
      */
     FencedBarrier,
     /**
-     * tubular lane marker, from "Barrier & Traffic Cone Pack"
+     * tubular lane marker, from "Barrier &amp; Traffic Cone Pack"
      */
     Marker,
     /**
-     * low barrier painted black, from "Barrier & Traffic Cone Pack"
+     * low barrier painted black, from "Barrier &amp; Traffic Cone Pack"
      */
     ShortBarrier,
     /**
-     * low barrier painted black and orange, from "Barrier & Traffic Cone Pack"
+     * low barrier painted black and orange, from "Barrier &amp; Traffic Cone
+     * Pack"
      */
     ShortBarrierPainted,
     /**
-     * low barrier painted black, orange, red, and white, from "Barrier &
+     * low barrier painted black, orange, red, and white, from "Barrier &amp;
      * Traffic Cone Pack"
      */
     ShortBarrierSigned,
     /**
-     * tall barrier painted black, from "Barrier & Traffic Cone Pack"
+     * tall barrier painted black, from "Barrier &amp; Traffic Cone Pack"
      */
     WallBarrier,
     /**
-     * tall barrier painted black and orange, from "Barrier & Traffic Cone Pack"
+     * tall barrier painted black and orange, from "Barrier &amp; Traffic Cone
+     * Pack"
      */
     WallBarrierPainted,
     /**
-     * free-standing warning sign, from "Barrier & Traffic Cone Pack"
+     * free-standing warning sign, from "Barrier &amp; Traffic Cone Pack"
      */
     WarningSign,
     /**
-     * iron weight, from "Barrier & Traffic Cone Pack"
+     * iron weight, from "Barrier &amp; Traffic Cone Pack"
      */
     Weight
 }
